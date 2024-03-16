2d25f3d45425584a8f9cdf532ef6c66116342c05
Clément Renault <clement@meilisearch.com>
2024-03-16 16:49:56 +0100

    Working version of the HTTP server

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,60 @@
-use std::env;
+use std::{env, str::FromStr};
 
-use axum::{routing::get, Router};
+use axum::{
+    body::{to_bytes, Body},
+    routing::post,
+    Router,
+};
+use flate2::bufread::GzDecoder;
 use pushover_rs::{send_pushover_request, MessageBuilder};
+use serde_json::{de::Deserializer, Value};
+use std::time::Duration;
+use tokio::net::TcpListener;
 
-mod tasks;
+const BODY_LIMIT: usize = 20 * 1024 * 1024; // 20 MiB
 
 #[tokio::main]
 async fn main() {
     let app = Router::new().route(
         "/",
-        get(|| async {
+        post(|body: Body| async {
+            let compressed_bytes = to_bytes(body, BODY_LIMIT).await.unwrap();
+            let bytes = GzDecoder::new(&compressed_bytes[..]);
+
             let pushover_user = env::var("PUSHOVER_USER").unwrap();
             let pushover_token = env::var("PUSHOVER_TOKEN").unwrap();
 
-            let message = MessageBuilder::new(&pushover_user, &pushover_token, "Example message")
-                .set_title("Example push notification sent through Pushover API")
-                .set_url("https://pushover.net/", Some("Pushover"))
-                .build();
+            for result in Deserializer::from_reader(bytes).into_iter() {
+                let value: Value = match result {
+                    Ok(value) => value,
+                    Err(e) => {
+                        eprintln!("{e:?}");
+                        continue;
+                    }
+                };
 
-            send_pushover_request(message).await.unwrap();
+                let uid = value.get("uid").unwrap().as_u64().unwrap();
+                let index_uid = value.get("indexUid").unwrap().as_str().unwrap();
+                let status = value.get("status").unwrap().as_str().unwrap();
+                let duration: Duration =
+                    iso8601::Duration::from_str(value.get("duration").unwrap().as_str().unwrap())
+                        .unwrap()
+                        .into();
+
+                let message = format!(
+                    "The task {uid} from {index_uid:?} {status} in processing in {duration:.02?}"
+                );
+
+                let message = MessageBuilder::new(&pushover_user, &pushover_token, &message)
+                    .set_title("A task just finished processing")
+                    .build();
+
+                send_pushover_request(message).await.unwrap();
+            }
         }),
     );
 
     // run our app with hyper, listening globally on port 3000
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    let listener = TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
 }
