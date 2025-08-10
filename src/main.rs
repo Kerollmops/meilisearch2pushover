5cda840e9446e64aa6eafba59b6f86db701c62d4
Clément Renault <clement@meilisearch.com>
2025-08-10 13:11:51 +0200

    Improve messaging

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{env, str::FromStr};
+use std::{env, sync::LazyLock};
 
 use axum::{
     body::{to_bytes, Body},
@@ -6,10 +6,13 @@ use axum::{
     Router,
 };
 use flate2::bufread::GzDecoder;
+use jiff::{SignedDuration, Unit};
 use pushover_rs::{send_pushover_request, MessageBuilder};
 use serde_json::{de::Deserializer, Value};
-use std::time::Duration;
 use tokio::net::TcpListener;
+
+static PUSHOVER_USER: LazyLock<String> = LazyLock::new(|| env::var("PUSHOVER_USER").unwrap());
+static PUSHOVER_TOKEN: LazyLock<String> = LazyLock::new(|| env::var("PUSHOVER_TOKEN").unwrap());
 
 const BODY_LIMIT: usize = 20 * 1024 * 1024; // 20 MiB
 
@@ -21,10 +24,7 @@ async fn main() {
             let compressed_bytes = to_bytes(body, BODY_LIMIT).await.unwrap();
             let bytes = GzDecoder::new(&compressed_bytes[..]);
 
-            let pushover_user = env::var("PUSHOVER_USER").unwrap();
-            let pushover_token = env::var("PUSHOVER_TOKEN").unwrap();
-
-            for result in Deserializer::from_reader(bytes).into_iter() {
+            for result in Deserializer::from_reader(bytes).into_iter().take(1) {
                 let value: Value = match result {
                     Ok(value) => value,
                     Err(e) => {
@@ -33,28 +33,61 @@ async fn main() {
                     }
                 };
 
-                let uid = value.get("uid").unwrap().as_u64().unwrap();
                 let index_uid = value.get("indexUid").and_then(|v| v.as_str());
                 let status = value.get("status").unwrap().as_str().unwrap();
-                let duration: Duration =
-                    iso8601::Duration::from_str(value.get("duration").unwrap().as_str().unwrap())
-                        .unwrap()
-                        .into();
+                let r#type = value.get("type").unwrap().as_str().unwrap();
+                let error_message = value.get("error.message").and_then(|v| v.as_str());
+                let indexed_documents = value
+                    .get("details.indexedDocuments")
+                    .and_then(|v| v.as_u64());
+                let received_documents = value
+                    .get("details.receivedDocuments")
+                    .and_then(|v| v.as_u64());
+                let duration = value
+                    .get("duration")
+                    .unwrap()
+                    .as_str()
+                    .unwrap()
+                    .parse::<SignedDuration>()
+                    .unwrap()
+                    .round(Unit::Second)
+                    .unwrap();
 
-                let message = match index_uid {
-                    Some(index_uid) => format!(
-                        "The task {uid} from {index_uid:?} {status} in processing in {duration:.02?}"
-                    ),
-                    None => format!(
-                        "The task {uid} {status} in processing in {duration:.02?}"
-                    ),
+                let message_builder = MessageBuilder::new(&PUSHOVER_USER, &PUSHOVER_TOKEN, "");
+                let message_builder = match (
+                    index_uid,
+                    error_message,
+                    received_documents,
+                    indexed_documents,
+                ) {
+                    (Some(index_uid), Some(error_message), Some(received_documents), _) => {
+                        message_builder
+                            .set_title(&format!(
+                                "Index {index_uid} failed indexing {received_documents} documents in {duration}"
+                            ))
+                          .modify_message(&format!("{}: {error_message}", r#type))
+                    }
+                    (Some(index_uid), None, _, Some(indexed_documents)) => {
+                        message_builder.set_title(&format!(
+                            "Index {index_uid} finished {status} {indexed_documents} documents in {duration}"
+                        ))
+                          .modify_message(r#type)
+                    },
+                    (None, _, _, _) => {
+                        message_builder.set_title(&format!("Indexing finished in {duration}"))
+                          .modify_message(r#type)
+                    },
+                    (Some(index_uid), _, _, _) => {
+                        message_builder.set_title(&format!(
+                            "Index {index_uid} finished {status} in {duration}"
+                        ))
+                          .modify_message(r#type)
+                    }
                 };
 
-                let message = MessageBuilder::new(&pushover_user, &pushover_token, &message)
-                    .set_title("A task just finished processing")
-                    .build();
-
-                send_pushover_request(message).await.unwrap();
+                send_pushover_request(message_builder.build())
+                    .await
+                    .unwrap();
             }
         }),
     );
