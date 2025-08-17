5a25af36ad2764d64f19ec5e51a7ffc8e7d90f01
Kerollmops <clement@meilisearch.com>
2025-08-17 15:56:13 +0200

    Improve notifications quality

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{env, sync::LazyLock};
+use std::{env, ops::Add, sync::LazyLock};
 
 use axum::{
     Router,
@@ -25,14 +25,21 @@ async fn main() {
             let compressed_bytes = to_bytes(body, BODY_LIMIT).await.unwrap();
             let bytes = GzDecoder::new(&compressed_bytes[..]);
 
-            for result in Deserializer::from_reader(bytes).into_iter().take(1) {
-                let TaskInfo { index_uid, status, r#type, error, details, duration } = match result {
-                    Ok(task) => task,
-                    Err(e) => {
-                        eprintln!("{e:?}");
-                        continue;
-                    }
-                };
+            let result = Deserializer::from_reader(bytes).into_iter().reduce(|acc, result| {
+                match (acc, result) {
+                    (Ok(acc), Ok(info)) => Ok(acc + info),
+                    (Err(e), _) | (_, Err(e)) => Err(e),
+                }
+            });
+
+            let TaskInfo { index_uid, status, r#type, error, details, duration } = match result {
+                Some(Ok(task)) => task,
+                Some(Err(e)) => {
+                    eprintln!("{e:?}");
+                    return;
+                },
+                None => return,
+            };
 
                 let duration = duration.round(jiff::Unit::Second).unwrap_or(duration);
                 let message_builder = MessageBuilder::new(&PUSHOVER_USER, &PUSHOVER_TOKEN, "");
@@ -50,10 +57,11 @@ async fn main() {
                           .modify_message(&format!("A {} {status} indexing {received_documents} documents in {duration:#}: {error_message}", r#type))
                     }
                     (Some(index_uid), None, _, Some(indexed_documents)) => {
+                        let speed = indexed_documents as f64 / duration.as_millis() as f64 / 1000.0;
                         message_builder.set_title(&format!(
                             "Index {index_uid} {status}"
                         ))
-                          .modify_message(&format!("A {} {status} indexing {indexed_documents} documents in {duration:#}", r#type))
+                          .modify_message(&format!("A {} {status} indexing {indexed_documents} documents in {duration:#} ({speed:.2} docs/s)", r#type))
                     },
                     (None, _, _, _) => {
                         message_builder.set_title(&format!("Task {status}"))
@@ -70,8 +78,7 @@ async fn main() {
                 send_pushover_request(message_builder.build())
                     .await
                     .unwrap();
-            }
-        }),
+        })
     );
 
     // run our app with hyper, listening globally on port 3000
@@ -101,4 +108,42 @@ struct TaskError {
 struct TaskDetails {
     indexed_documents: Option<u64>,
     received_documents: Option<u64>,
+}
+
+impl Add for TaskInfo {
+    type Output = Self;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        TaskInfo {
+            index_uid: self.index_uid.or(rhs.index_uid),
+            status: self.status,
+            r#type: self.r#type,
+            error: self.error.or(rhs.error),
+            details: match (self.details, rhs.details) {
+                (Some(a), Some(b)) => Some(a + b),
+                (Some(x), _) | (_, Some(x)) => Some(x),
+                (None, None) => None,
+            },
+            duration: self.duration,
+        }
+    }
+}
+
+impl Add for TaskDetails {
+    type Output = Self;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        TaskDetails {
+            indexed_documents: match (self.indexed_documents, rhs.indexed_documents) {
+                (Some(a), Some(b)) => Some(a + b),
+                (Some(x), _) | (_, Some(x)) => Some(x),
+                (None, None) => None,
+            },
+            received_documents: match (self.received_documents, rhs.received_documents) {
+                (Some(a), Some(b)) => Some(a + b),
+                (Some(x), _) | (_, Some(x)) => Some(x),
+                (None, None) => None,
+            },
+        }
+    }
 }
